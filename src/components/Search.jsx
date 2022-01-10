20e6ed1d479061820df6cfa06e5e2220630b8d29
Alexmask <alexismaskio@gmail.com>
2022-01-10 11:29:09 +0100

    autre commit test

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,7 +7,7 @@ const data = new Array(8).fill({
     description: 'Description for Item',
 });
 
-// Test
+// Test dedee
 const renderIconTags = (props) => (
     <Icon name='pricetags-outline' {...props} />
 );
