4393ef50f321a00f1d13710bae65a0bfbab2c756
Alexmask <alexismaskio@gmail.com>
2022-01-12 11:27:52 +0100

    ajout de la carte, package localisation

--- src/components/Carte.js
+++ src/components/Carte.js
@@ -1,99 +1,72 @@
 import React, { useState, useEffect } from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions, FlatList} from 'react-native';
-import LocalisationListItem from "../components/LocalisationListItem";
+import { StyleSheet, Text, View, Dimensions, FlatList } from 'react-native';
+// import GetLocation from 'react-native-get-location'
+import * as Location from 'expo-location';
+import { Layout, Divider, List, ListItem, Button, Input, Select, SelectItem, IndexPath, Icon } from '@ui-kitten/components';
 
 
+const Carte = ({ navigation, localisation }) => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [placement, setPlacement] = useState(null);
 
-const Carte = ({ navigation}) => {
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
 
-  const localisation = [
-  {
-      "id" : 1,
-      "nom": "tchanville",
-      "loc" : "Le petit marcel",
-      "coordonnee": {
-        "latitude": 39.6762,
-        "longitude": 45.6503,
-        "latitudeDelta": 1,
-        "longitudeDelta": 1,
-      },
-      'description' : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod hendrerit felis. Donec sed consequat tortor. Nam a nisi semper, gravida orci nec, cursus ex. Duis id laoreet neque. Vivamus ut volutpat neque, sit amet condimentum neque. Vestibulum ut iaculis sapien. Nunc facilisis faucibus nisi in accumsan. Phasellus a quam non risus sollicitudin tempus. Ut dapibus quam nunc, eget congue dui tincidunt sed. Sed tempus risus sit amet urna auctor venenatis. Mauris et odio at mi lacinia elementum eget nec lectus. Maecenas ipsum mi, elementum quis egestas quis, feugiat eu nibh. Donec vel urna vel ipsum sagittis tincidunt. Mauris vehicula scelerisque tortor, id auctor ligula pharetra a. Duis tristique nulla turpis, id molestie massa vehicula vel. "
-  },
-  {
-    "id" : 2,
-    "nom" : "la feuille d'or",
-    "loc" : "Metz",
-    "coordonnee": {
-      "latitude": 45.6762,
-      "longitude": 100.6503,
-      "latitudeDelta": 1,
-      "longitudeDelta": 1,
-    },
-    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod hendrerit felis. Donec sed consequat tortor. Nam a nisi semper, gravida orci nec, cursus ex. Duis id laoreet neque. Vivamus ut volutpat neque, sit amet condimentum neque. Vestibulum ut iaculis sapien. Nunc facilisis faucibus nisi in accumsan. Phasellus a quam non risus sollicitudin tempus. Ut dapibus quam nunc, eget congue dui tincidunt sed. Sed tempus risus sit amet urna auctor venenatis. Mauris et odio at mi lacinia elementum eget nec lectus. Maecenas ipsum mi, elementum quis egestas quis, feugiat eu nibh. Donec vel urna vel ipsum sagittis tincidunt. Mauris vehicula scelerisque tortor, id auctor ligula pharetra a. Duis tristique nulla turpis, id molestie massa vehicula vel. "
-},
-{
-  "id" : 3,
-  "nom" : "le cirque zavatta",
-  "loc" : "Paris",
-  "coordonnee": {
-    "latitude": 48.856614,
-    "longitude": 2.3522219,
-    "latitudeDelta": 1,
-    "longitudeDelta": 1,
-  },
-  "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod hendrerit felis. Donec sed consequat tortor. Nam a nisi semper, gravida orci nec, cursus ex. Duis id laoreet neque. Vivamus ut volutpat neque, sit amet condimentum neque. Vestibulum ut iaculis sapien. Nunc facilisis faucibus nisi in accumsan. Phasellus a quam non risus sollicitudin tempus. Ut dapibus quam nunc, eget congue dui tincidunt sed. Sed tempus risus sit amet urna auctor venenatis. Mauris et odio at mi lacinia elementum eget nec lectus. Maecenas ipsum mi, elementum quis egestas quis, feugiat eu nibh. Donec vel urna vel ipsum sagittis tincidunt. Mauris vehicula scelerisque tortor, id auctor ligula pharetra a. Duis tristique nulla turpis, id molestie massa vehicula vel. "
-},
-  ];
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+    console.log(text);
+  } else if (location) {
+    text = JSON.stringify(location);
+    console.log(text);
+  }
 
   const tokyoRegion = {
-
     latitude: 35.6762,
     longitude: 139.6503,
     latitudeDelta: 1,
     longitudeDelta: 1,
   };
 
+
+
   const navigateToLocalisationDetails = (localisationID) => {
-    navigation.navigate("ViewLocalisationDetail", {localisationID});
+    navigation.navigate("ViewLocalisationDetail", { localisationID });
   };
 
-
-  
-  
- 
+  const navigateToAddNewPlace = () => {
+    navigation.navigate("Add New Place");
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.fenetremap}>
 
-        <MapView style={styles.map}
-          initialRegion={tokyoRegion}
-        >
-           <Marker coordinate={tokyoRegion} />
+    <MapView style={styles.map}
+      initialRegion={tokyoRegion}
+    >
+      <Marker coordinate={tokyoRegion} />
 
 
-          {localisation.map((localisation ) => (
-            <Marker
-              key={localisation.id}
-              coordinate={localisation.coordonnee}
-              title={localisation.nom}
-              description={localisation.description}
-            />
-          ))}
-        </MapView>
-
-        <FlatList
-        data={localisation}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <LocalisationListItem LocalisationData={item} onClick={navigateToLocalisationDetails}/>
-        )}
+      {localisation.map((localisation) => (
+        <Marker
+          key={localisation.id}
+          coordinate={localisation.coordonnee}
+          title={localisation.nom}
+          description={localisation.description}
         />
-      </View>
-    
-
-    </View>
+      ))}
+      <Text>{text}</Text>
+    </MapView>
   )
 };
 
--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,36 +1,82 @@
 import React, { useState } from 'react';
-import { Layout, Text, Button } from '@ui-kitten/components';
-import { StyleSheet } from 'react-native';
+import { Layout, Text, Button, List, Divider, ListItem } from '@ui-kitten/components';
+import { StyleSheet, View, Dimensions, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 
-const Places = ({ navigation, testVariable, dispatch}) => {
+import Carte from "./Carte";
 
-    const [title, setTitle] = useState(testVariable);
-    const ChangeTitle = () => {
-        const action = { type: 'CHANGE_TITLE' };
-        dispatch(action);
-    }
+const Places = ({ navigation, testVariable, dispatch }) => {
+
+    const localisation = [
+        {
+            "id": 1,
+            "nom": "tchanville",
+            "loc": "Le petit marcel",
+            "coordonnee": {
+                "latitude": 39.6762,
+                "longitude": 45.6503,
+                "latitudeDelta": 1,
+                "longitudeDelta": 1,
+            },
+            'description': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod hendrerit felis. Donec sed consequat tortor. Nam a nisi semper, gravida orci nec, cursus ex. Duis id laoreet neque. Vivamus ut volutpat neque, sit amet condimentum neque. Vestibulum ut iaculis sapien. Nunc facilisis faucibus nisi in accumsan. Phasellus a quam non risus sollicitudin tempus. Ut dapibus quam nunc, eget congue dui tincidunt sed. Sed tempus risus sit amet urna auctor venenatis. Mauris et odio at mi lacinia elementum eget nec lectus. Maecenas ipsum mi, elementum quis egestas quis, feugiat eu nibh. Donec vel urna vel ipsum sagittis tincidunt. Mauris vehicula scelerisque tortor, id auctor ligula pharetra a. Duis tristique nulla turpis, id molestie massa vehicula vel. "
+        },
+        {
+            "id": 2,
+            "nom": "la feuille d'or",
+            "loc": "Metz",
+            "coordonnee": {
+                "latitude": 45.6762,
+                "longitude": 100.6503,
+                "latitudeDelta": 1,
+                "longitudeDelta": 1,
+            },
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod hendrerit felis. Donec sed consequat tortor. Nam a nisi semper, gravida orci nec, cursus ex. Duis id laoreet neque. Vivamus ut volutpat neque, sit amet condimentum neque. Vestibulum ut iaculis sapien. Nunc facilisis faucibus nisi in accumsan. Phasellus a quam non risus sollicitudin tempus. Ut dapibus quam nunc, eget congue dui tincidunt sed. Sed tempus risus sit amet urna auctor venenatis. Mauris et odio at mi lacinia elementum eget nec lectus. Maecenas ipsum mi, elementum quis egestas quis, feugiat eu nibh. Donec vel urna vel ipsum sagittis tincidunt. Mauris vehicula scelerisque tortor, id auctor ligula pharetra a. Duis tristique nulla turpis, id molestie massa vehicula vel. "
+        },
+        {
+            "id": 3,
+            "nom": "le cirque zavatta",
+            "loc": "Paris",
+            "coordonnee": {
+                "latitude": 48.856614,
+                "longitude": 2.3522219,
+                "latitudeDelta": 1,
+                "longitudeDelta": 1,
+            },
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod hendrerit felis. Donec sed consequat tortor. Nam a nisi semper, gravida orci nec, cursus ex. Duis id laoreet neque. Vivamus ut volutpat neque, sit amet condimentum neque. Vestibulum ut iaculis sapien. Nunc facilisis faucibus nisi in accumsan. Phasellus a quam non risus sollicitudin tempus. Ut dapibus quam nunc, eget congue dui tincidunt sed. Sed tempus risus sit amet urna auctor venenatis. Mauris et odio at mi lacinia elementum eget nec lectus. Maecenas ipsum mi, elementum quis egestas quis, feugiat eu nibh. Donec vel urna vel ipsum sagittis tincidunt. Mauris vehicula scelerisque tortor, id auctor ligula pharetra a. Duis tristique nulla turpis, id molestie massa vehicula vel. "
+        },
+    ];
+
+    const renderItem = ({ item, index }) => (
+        <ListItem
+            title={`${item.loc} ${index + 1}`}
+            description={`${item.nom} ${index + 1}`}
+            // accessoryRight={renderItemAccessory}
+            onPress={item => navigateToLocalisationDetails(item.id)}
+        />
+    );
 
     const navigateToAddNewPlace = () => {
         navigation.navigate("Add New Place");
-      };
+    };
 
-      const navigateToMap = () => {
-        navigation.navigate("ViewCarte");
-      };
+    const navigateToLocalisationDetails = (localisationID) => {
+        navigation.navigate("ViewLocalisationDetail", { localisationID });
+    };
 
     return (
         <Layout style={styles.container}>
-            <Text category='h1'>{testVariable}</Text>
-            <Button onPress={ChangeTitle}>
-                Changer le titre
-            </Button>
-            <Button onPress={navigateToAddNewPlace}>
-                Add New Place
-            </Button>
-            <Button onPress={navigateToMap}>
-                Allez sur la carte
-            </Button>
+            <View style={styles.fenetremap}>
+                <Carte localisation={localisation} />
+                <Button style={styles.space} onPress={navigateToAddNewPlace}>
+                    Add New Place
+                </Button>
+                <List
+                    data={localisation}
+                    // keyExtractor={(item) => item.id.toString()}
+                    ItemSeparatorComponent={Divider}
+                    renderItem={renderItem}
+                />
+            </View>
         </Layout>
     );
 };
@@ -46,7 +92,13 @@ export default connect(mapStateToProps)(Places);
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
-    }
+        justifyContent: 'center',
+        paddingVertical: 0
+    },
+    subcontainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
