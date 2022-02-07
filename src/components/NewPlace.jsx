410547092dbd60f21ff30ac051e2f9bbff7e16aa
jim <daftjim1@gmail.com>
2022-02-07 23:07:01 +0100

    Control forms OK !

--- src/components/NewPlace.jsx
+++ src/components/NewPlace.jsx
@@ -7,8 +7,15 @@ import { connect } from 'react-redux';
 import { autoComplete, geocoding } from '../api/Here';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const NewPlace = ({ placesList, dispatch, navigation, route }) => {
 
+const NewPlace = ({ placesList, dispatch, navigation, route }) => {
+    
+    //style du formulaire
+    const [nameInput, setNameInput] = useState('basic');
+    const [descInput, setDescInput] = useState('basic');
+    const [addrInput, setAddrInput] = useState('basic');
+    
+    //liste des tags de la page Tags
     useEffect(() => {
         if(route.params?.list) {
             //console.log(route.params.list);
@@ -36,11 +43,78 @@ const NewPlace = ({ placesList, dispatch, navigation, route }) => {
         setAddressData(res.items);
     };
 
+    //lors d'une saisie sur le champ adresse
+    const onChangeText = (query) => {
+        setAddress(query);
+        setAddrInput('basic');  //mis a jour style formulaire
+        if(address != '')
+            fetchAddress();
+    };
 
-    const [selectedIndex, setSelectedIndex] = React.useState([
-        new IndexPath(0),
-        new IndexPath(1),
-    ]);
+    //choix autocompletion sur le champ adresse
+    const onSelect = (index) => {
+        setAddress(addressData[index].address.label);
+    };
+
+    //DEBUG ONLY
+    const resetPlace = () => {
+        const action = {type: 'RESET_PLACE'};
+        dispatch(action);
+    } 
+
+    //verification du formulaire
+    const verifForm = () => {
+        let valid = true;
+
+        if(name.trim().length == 0) {
+            setNameInput('danger');
+            valid = false;
+        }
+        if(address.trim().length == 0) {
+            setAddrInput('danger');
+            valid = false;
+        }
+        return valid;
+    };
+    
+    //enregistrement d'un lieu
+    const addPlace = async () => {
+        //TODO verification retour API
+
+        if(verifForm()) {
+            const res = await geocoding(address);
+    
+            //constitution de notre objet Lieu
+            //id mis a jour ds reducer
+            const newPlace = {
+                "id": 0,
+                "nom": name,
+                "loc": address,
+                "coordonnee": {
+                    "latitude": res.items[0].position.lat,
+                    "longitude": res.items[0].position.lng,
+                    "latitudeDelta": 1,
+                    "longitudeDelta": 1,
+                },
+                "description": description,
+                "tags": tags
+            };
+            const action = {type: 'ADD_PLACE', value: newPlace};
+            dispatch(action);
+    
+            Toast.show('Place saved !', {
+                duration: Toast.durations.LONG,
+            });
+    
+            navigation.navigate("Places");
+        }
+        else {
+            Toast.show('Form incomplete !', {
+                duration: Toast.durations.SHORT,
+            });
+        }
+        
+    };
 
     // Icône Tags
     const renderIconTags = (props) => (
@@ -57,24 +131,6 @@ const NewPlace = ({ placesList, dispatch, navigation, route }) => {
         <Icon name='edit-outline' {...props} />
     );
 
-    //lors d'une saisie sur le champ adresse
-    const onChangeText = (query) => {
-        setAddress(query);
-        if(address != '')
-            fetchAddress();
-    };
-
-    //choix autocompletion sur le champ adresse
-    const onSelect = (index) => {
-        setAddress(addressData[index].address.label);
-    };
-
-    //DEBUG ONLY
-    const resetPlace = () => {
-        const action = {type: 'RESET_PLACE'};
-        dispatch(action);
-    }
-
     //affichage autocompletion adresse
     const renderAutocomplete = (item, index) => (
         <AutocompleteItem
@@ -82,39 +138,8 @@ const NewPlace = ({ placesList, dispatch, navigation, route }) => {
             title={item.address.label}
         />
     );
-    
-    //enregistrement d'un lieu
-    const addPlace = async () => {
-        //TODO verification formulaire
-        //TODO verification retour API
-        
-        const res = await geocoding(address);
 
-        //constitution de notre objet Lieu
-        //id mis a jour ds reducer
-        const newPlace = {
-            "id": 0,
-            "nom": name,
-            "loc": address,
-            "coordonnee": {
-                "latitude": res.items[0].position.lat,
-                "longitude": res.items[0].position.lng,
-                "latitudeDelta": 1,
-                "longitudeDelta": 1,
-            },
-            "description": description,
-            "tags": tags
-        };
-        const action = {type: 'ADD_PLACE', value: newPlace};
-        dispatch(action);
-
-        let toast = Toast.show('Place saved !', {
-            duration: Toast.durations.LONG,
-        });
-
-        navigation.navigate("Places");
-    };
-
+    //affichage des tags
     const renderTags = ({item, index}) => (
         <ListItem
             key={index}
@@ -125,11 +150,12 @@ const NewPlace = ({ placesList, dispatch, navigation, route }) => {
     return (
         <Layout style={styles.container}>
             <Input
-                placeholder='Name'
+                placeholder='Name *'
                 accessoryLeft={renderIconText}
                 value={name}
-                onChangeText={nextName => setName(nextName)}
+                onChangeText={nextName => {setName(nextName); setNameInput('basic')}}
                 style={styles.input}
+                status={nameInput}
             />
             <Input
                 multiline={true}
@@ -139,6 +165,7 @@ const NewPlace = ({ placesList, dispatch, navigation, route }) => {
                 value={description}
                 onChangeText={nextDescription => setDescription(nextDescription)}
                 style={styles.input}
+                status={descInput}
             />
             <TouchableOpacity style={styles.tagList} onPress={() => {navigation.navigate("Tags", {list: tags})}}>
                 <Text>
@@ -154,12 +181,13 @@ const NewPlace = ({ placesList, dispatch, navigation, route }) => {
                 }
             </TouchableOpacity>
             <Autocomplete
-                placeholder='Address'
+                placeholder='Address *'
                 value={address}
                 onSelect={onSelect}
                 onChangeText={onChangeText}
                 accessoryLeft={renderIconAddress}
-                style={styles.input}>
+                style={styles.input}
+                status={addrInput}>
                 {addressData.map(renderAutocomplete)}
             </Autocomplete>
             <Button onPress={addPlace}>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,6 +5,8 @@ import { StyleSheet} from 'react-native';
 import { cityAutoComplete, geocoding } from '../api/Here';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faTachometerAlt } from '@fortawesome/free-solid-svg-icons';
+import Toast from 'react-native-root-toast';
+
 
 //distance en km
 const distanceData = [
@@ -13,6 +15,8 @@ const distanceData = [
 
 const Search = ({navigation, placesList, tagsList}) => {
 
+    const [cityInput, setCityinput] = useState('basic'); //style du champ ville
+
     //resultat de la recherche
     const [searchList, setSearchList] = useState([]);
     //resultat api autocompletion ville
@@ -20,28 +24,16 @@ const Search = ({navigation, placesList, tagsList}) => {
     // Entrées de l'utilisateur
     const [search, setSearch] = useState('');
     const [city, setCity] = useState('');
+
     //values select components
     const [selectedIndexTags, setSelectedIndexTags] = React.useState([
         new IndexPath(0),
         new IndexPath(1),
     ]);
-    const [selectedIndexKilometer, setSelectedIndexKilometer] = React.useState([
-        new IndexPath(0),
-        new IndexPath(1),
-    ]);
+    const [selectedIndexKilometer, setSelectedIndexKilometer] = React.useState(
+        new IndexPath(0));
     //tab des tags selectionnes
     const tagValues = selectedIndexTags.map(index => tagsList[index.row].name);
-    
-    //icones
-    const renderIconTags = (props) => (
-        <Icon name='pricetags-outline' {...props} />
-    );
-    const renderIconAddress = (props) => (
-        <Icon name='pin-outline' {...props} />
-    );
-    const renderIconSearch = (props) => (
-        <Icon name='search-outline' {...props} />
-    );
 
     //choix autocompletion sur le champ city
     const onSelectCity = (index) => {
@@ -58,6 +50,7 @@ const Search = ({navigation, placesList, tagsList}) => {
     //appel api pour avoir liste autocompletion city
     const fetchCity = async (query) => {
         setCityList([]);
+        setCityinput('basic');
         const res = await cityAutoComplete(query);
         setCityList(res.items);
     };
@@ -65,7 +58,79 @@ const Search = ({navigation, placesList, tagsList}) => {
     //navigation vers detail
     const navigateToLocalisationDetails = (index) => {
         navigation.navigate("ViewPlacesDetails", { index });
+    }; 
+    
+    //verification formulaire
+    const verifForm = () => {
+        let valid = true;
+
+        if(city.trim().length == 0) {
+            setCityinput('danger');
+            valid = false;
+        }
+        return valid;
     };
+
+    //recherche des lieux
+    const btnSearch = async () => {
+        //TODO verification formulaire
+        //TODO verification retour API
+
+        if(verifForm()) {
+            /*const res = await geocoding(city);
+    
+            let filter = [];
+            
+            //filtre distance (si distance entre coord lieu et coord recherche < distance max)
+            filter = placesList.filter(place => distance(res.items[0].position.lat, res.items[0].position.lng, place.coordonnee.latitude, place.coordonnee.longitude) <= distanceData[selectedIndexKilometer.row]);
+    
+            //filtre champ search
+            if (search.length > 0) 
+                filter = filter.filter(place => place.nom.toLowerCase().includes(search.toLowerCase()));
+    
+            //filtre tag (si lieux contient tags selectionnes)
+            if(tagValues.length > 0)
+                filter = filter.filter(place => place.tags.some(tag => tagValues.includes(tag.name)));
+    
+            setSearchList(filter);*/
+        }
+        else {
+            Toast.show('Form incomplete !', {
+                duration: Toast.durations.SHORT,
+            });
+        }
+        
+    };
+
+    //Calcule la distance entre 2 coord GPS
+    const distance = (lat1, lon1, lat2, lon2) => {
+        var radlat1 = Math.PI * lat1/180;
+        var radlat2 = Math.PI * lat2/180;
+        var theta = lon1-lon2;
+        var radtheta = Math.PI * theta/180;
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180/Math.PI;
+        dist = dist * 60 * 1.1515;
+        
+        dist = dist * 1.609344; //conversion en km
+
+        return dist;
+    };
+
+    //icones
+    const renderIconTags = (props) => (
+        <Icon name='pricetags-outline' {...props} />
+    );
+    const renderIconAddress = (props) => (
+        <Icon name='pin-outline' {...props} />
+    );
+    const renderIconSearch = (props) => (
+        <Icon name='search-outline' {...props} />
+    );
 
     //affichage autocompletion city
     const renderAutocomplete = (item, index) => (
@@ -95,48 +160,6 @@ const Search = ({navigation, placesList, tagsList}) => {
         <SelectItem title={item + ' km'} key={index}/>
     );
 
-    //recherche des lieux
-    const btnSearch = async () => {
-        //TODO verification formulaire
-        //TODO verification retour API
-        
-        const res = await geocoding(city);
-
-        let filter = [];
-        
-        //filtre distance (si distance entre coord lieu et coord recherche < distance max)
-        filter = placesList.filter(place => distance(res.items[0].position.lat, res.items[0].position.lng, place.coordonnee.latitude, place.coordonnee.longitude) <= distanceData[selectedIndexKilometer.row]);
-
-        //filtre champ search
-        if (search.length > 0) 
-            filter = filter.filter(place => place.nom.toLowerCase().includes(search.toLowerCase()));
-
-        //filtre tag (si lieux contient tags selectionnes)
-        if(tagValues.length > 0)
-            filter = filter.filter(place => place.tags.some(tag => tagValues.includes(tag.name)));
-
-        setSearchList(filter);
-    };
-
-    //Calcule la distance entre 2 coord GPS
-    const distance = (lat1, lon1, lat2, lon2) => {
-        var radlat1 = Math.PI * lat1/180;
-        var radlat2 = Math.PI * lat2/180;
-        var theta = lon1-lon2;
-        var radtheta = Math.PI * theta/180;
-        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-        if (dist > 1) {
-            dist = 1;
-        }
-        dist = Math.acos(dist);
-        dist = dist * 180/Math.PI;
-        dist = dist * 60 * 1.1515;
-        
-        dist = dist * 1.609344; //conversion en km
-
-        return dist;
-    };
-
     return (
         <Layout style={styles.container}>
             <Layout style={styles.containerSearch}>
@@ -159,12 +182,13 @@ const Search = ({navigation, placesList, tagsList}) => {
                 </Select>
                 <Layout>
                     <Autocomplete
-                        placeholder='City'
+                        placeholder='City *'
                         value={city}
                         onSelect={onSelectCity}
                         onChangeText={onChangeText}
                         accessoryLeft={renderIconAddress}
-                        style={styles.input}>
+                        style={styles.input}
+                        status={cityInput}>
                         {cityList.map(renderAutocomplete)}
                     </Autocomplete>
                     <Select
